4436b54b1fceb0fc1cacada23bfb5a6892ba4540
Aleksander Rokita <kelor2@o2.pl>
2025-05-09 14:11:43 +0200

    Change admin panel display

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -12,29 +12,35 @@ from task_manager.models import (
     Task,
 )
 
-# Register your models here.
-
 admin.site.unregister(Group)
 
 
 @admin.register(TaskType)
 class TaskTypeAdmin(admin.ModelAdmin):
-    list_display = ["name"]
+    list_display = ("name",)
 
 
 @admin.register(Position)
 class PositionAdmin(admin.ModelAdmin):
-    list_display = ["name"]
+    list_display = ("name",)
 
 
 @admin.register(Team)
 class TeamAdmin(admin.ModelAdmin):
-    list_display = ["name"]
+    list_display = ("name",)
 
 
 @admin.register(Worker)
 class WorkerAdmin(UserAdmin):
     list_display = UserAdmin.list_display + ("position", "team")
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("position", "team")}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Personal info", {"fields": ("first_name", "last_name", "email")}),
+        ("Additional info", {"fields": ("position", "team")}),
+    )
 
 
 @admin.register(Project)
@@ -42,13 +48,50 @@ class ProjectAdmin(admin.ModelAdmin):
     list_display = ("name", "description", "deadline", "team")
 
 
+class WorkerInline(admin.TabularInline):
+    model = Worker.tasks.through
+    extra = 0
+    show_change_link = True
+
+
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = (
+    list_display = [
         "name",
         "description",
         "deadline",
         "is_completed",
         "priority",
         "task_type",
+    ]
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "name",
+                    "description",
+                    "deadline",
+                    "is_completed",
+                    "priority",
+                    "task_type",
+                )
+            },
+        ),
     )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "name",
+                    "description",
+                    "deadline",
+                    "is_completed",
+                    "priority",
+                    "task_type",
+                )
+            },
+        ),
+    )
+    inlines = [WorkerInline]
