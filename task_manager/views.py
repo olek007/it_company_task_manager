eb597d4cc81cda5a3a544cef83575e3ce9c3ebce
Aleksander Rokita <kelor2@o2.pl>
2025-05-23 12:22:12 +0200

    Change context object names Optimize queries

--- task_manager/views.py
+++ task_manager/views.py
@@ -28,6 +28,7 @@ class SignUpView(CreateView):
 class TaskTypeListView(LoginRequiredMixin, ListView):
     model = TaskType
     template_name = "task_manager/task_type_list.html"
+    context_object_name = "task_type_list"
 
 
 class TaskTypeDetailView(LoginRequiredMixin, DetailView):
@@ -56,33 +57,46 @@ class TeamListView(LoginRequiredMixin, ListView):
 class TeamDetailView(LoginRequiredMixin, DetailView):
     model = Team
     template_name = "task_manager/team_detail.html"
+    queryset = (
+        Team.objects.prefetch_related("projects").prefetch_related("workers").all()
+    )
 
 
 class WorkerListView(LoginRequiredMixin, ListView):
     model = Worker
     template_name = "task_manager/worker_list.html"
+    queryset = Worker.objects.select_related("team").prefetch_related("tasks").all()
 
 
 class WorkerDetailView(LoginRequiredMixin, DetailView):
     model = Worker
     template_name = "task_manager/worker_detail.html"
+    queryset = Worker.objects.select_related("team").prefetch_related("tasks").all()
 
 
 class ProjectListView(LoginRequiredMixin, ListView):
     model = Project
     template_name = "task_manager/project_list.html"
+    queryset = Project.objects.select_related("team").prefetch_related("tasks").all()
 
 
 class ProjectDetailView(LoginRequiredMixin, DetailView):
     model = Project
     template_name = "task_manager/project_detail.html"
+    queryset = Project.objects.select_related("team").prefetch_related("tasks").all()
 
 
 class TaskListView(LoginRequiredMixin, ListView):
     model = Task
     template_name = "task_manager/task_list.html"
+    queryset = (
+        Task.objects.select_related("project").prefetch_related("assignees").all()
+    )
 
 
 class TaskDetailView(LoginRequiredMixin, DetailView):
     model = Task
     template_name = "task_manager/task_detail.html"
+    queryset = (
+        Task.objects.select_related("project").prefetch_related("assignees").all()
+    )
