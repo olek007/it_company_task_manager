0e5b0382b99add57c2987168b648cf620dfc33c5
Aleksander Rokita <kelor2@o2.pl>
2025-05-23 10:31:16 +0200

    Change urls naming

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -12,11 +12,11 @@ from task_manager.views import (
 
 urlpatterns = [
     path("", index, name="index"),
-    path("taskType/", TaskTypeListView.as_view(), name="task_type_list"),
-    path("position/", PositionListView.as_view(), name="position_list"),
-    path("team/", TeamListView.as_view(), name="team_list"),
-    path("worker/", WorkerListView.as_view(), name="worker_list"),
-    path("project/", ProjectListView.as_view(), name="project_list"),
-    path("task/", TaskListView.as_view(), name="task_list"),
+    path("task_types/", TaskTypeListView.as_view(), name="task_type-list"),
+    path("positions/", PositionListView.as_view(), name="position-list"),
+    path("teams/", TeamListView.as_view(), name="team-list"),
+    path("workers/", WorkerListView.as_view(), name="worker-list"),
+    path("projects/", ProjectListView.as_view(), name="project-list"),
+    path("tasks/", TaskListView.as_view(), name="task-list"),
 ]
 app_name = "task_manager"
