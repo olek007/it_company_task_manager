06d1611e600e208c9657aeba12e4b8f2d86c7f68
Aleksander Rokita <kelor2@o2.pl>
2025-06-09 16:42:59 +0200

    Add models tests

---
+++ task_manager/utilities.py
@@ -0,0 +1,10 @@
+from enum import StrEnum
+
+
+class Orderings(StrEnum):
+    NAME_ASC = "name_asc"
+    NAME_DESC = "name_desc"
+    DEADLINE_ASC = "deadline_asc"
+    DEADLINE_DESC = "deadline_desc"
+    USERNAME_ASC = "username_asc"
+    USERNAME_DESC = "username_desc"
